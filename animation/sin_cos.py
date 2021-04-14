63da857e865dd245e3433c912e4c5fd402f75933
leadpogrommer <ilyamerzlakov@gmail.com>
2021-04-14 23:58:46 +0700

    improved physics

---
+++ animation/sin_cos.py
@@ -0,0 +1,32 @@
+import math
+
+
+def write_image(filename, arr):
+    f = open(filename, mode='wb')
+    f.write(bytes("v2.0 raw\n", 'UTF-8'))
+    for byte in arr:
+        f.write(bytes('%08x' % byte + '\n', 'UTF-8'))
+    f.close()
+
+
+speed = 2
+max_angle = math.pi/3
+sins = []
+for i in range(0, 128):
+    s = math.sin(i / 127 * max_angle) * speed
+    # print(round(s*128))
+    sins.append(round(s*128))
+write_image('sin.img', sins)
+
+
+coss = []
+for i in range(0, 128):
+    s = math.cos(i / 127 * max_angle) * speed
+    # print(round(s*128))
+    coss.append(round(s*128))
+write_image('cos.img', coss)
+
+
+
+for i in range(0, 128):
+    print(((sins[i]/128)**2 + (coss[i]/128)**2)**0.5)
